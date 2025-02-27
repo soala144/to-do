85e6dabeae6e952d34cd2630aa00ed15a83d132e
soala144 <soala144@gmail.com>
2025-02-27 05:33:08 -0800

    Done with dark and light mode

--- src/components/ThemeIcon.jsx
+++ src/components/ThemeIcon.jsx
@@ -1,8 +1,26 @@
+import { useState, useEffect } from "react";
 const ThemeIcon = () => {
+  const getTheme = () => {
+    const theme = localStorage.getItem("Theme");
+    return theme === "dark" ? "dark" : "light";
+  };
+
+  const [theme, setTheme] = useState(getTheme());
+  useEffect(() => {
+    document.body.setAttribute("dark-theme", theme);
+    localStorage.setItem("Theme", theme);
+  }, [theme]);
+  const handleThemeSwitch = () => {
+    setTheme((prevTheme) => (prevTheme === "light" ? "dark" : "light"));
+  };
   return (
-    <div>
-      <img src="/public/images/icon-moon.svg" alt="" />
-    </div>
+    <figure className="theme-switch-icon" onClick={handleThemeSwitch}>
+      {theme === "dark" ? (
+        <img src="/images/icon-sun.svg" alt="light mode icon" />
+      ) : (
+        <img src="/images/icon-moon.svg" alt="dark mode icon" />
+      )}
+    </figure>
   );
 };
 
