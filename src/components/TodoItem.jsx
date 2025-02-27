44d7ab37816ef1fa5e58cc98a261c40715113737
soala144 <soala144@gmail.com>
2025-02-26 17:05:23 -0800

    Implemented completed task

--- src/components/TodoItem.jsx
+++ src/components/TodoItem.jsx
@@ -1,6 +1,13 @@
 import React from "react";
 
 const TodoItem = ({ todoItem, todoList, onTodolist }) => {
+  const handleToggleTodo = (id) => {
+    onTodolist((todoList) =>
+      todoList.map((todo) =>
+        id === todo.id ? { ...todo, completed: !todoItem.completed } : todo
+      )
+    );
+  };
   const handleDeleteTodo = (id) => {
     onTodolist(todoList.filter((todo) => todo.id !== id));
   };
@@ -8,10 +15,20 @@ const TodoItem = ({ todoItem, todoList, onTodolist }) => {
     <>
       <div className="todos">
         <div className="todo-item">
-          <figure className="check-icon">
-            <img src="images/icon-check.svg" alt="check icon" />
+          <figure
+            className={`check-icon ${
+              todoItem.completed && "checked-background"
+            }`}
+            onClick={() => handleToggleTodo(todoItem.id)}
+          >
+            {todoItem.completed && (
+              <img src="images/icon-check.svg" alt="check icon" />
+            )}
           </figure>
-          <p className="todo-text"> {todoItem.text}</p>
+          <p className={`todo-text ${todoItem.completed && "checked-text"}`}>
+            {" "}
+            {todoItem.text}
+          </p>
         </div>
         <figure
           className="cancel-icon"
